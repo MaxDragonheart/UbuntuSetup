c588c526ce26bd70fcf7f9e602b59513760a8ba4
Massimiliano Moraca <massimilianomoraca@gmail.com>
2022-09-16 17:51:53 +0200

    Create qgis_installation.sh

---
+++ qgis_installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Install QGIS | START"
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install gnupg software-properties-common
+
+sudo mkdir -m755 -p /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/qgis-archive-keyring.gpg https://download.qgis.org/downloads/qgis-archive-keyring.gpg
+sudo cp qgis.sources /etc/apt/sources.list.d/
+
+sudo apt update -y
+sudo apt install -y qgis qgis-plugin-grass
+
+qgis --version
+
+echo "Install QGIS | END"
