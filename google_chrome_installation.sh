e0b02c0c7902e22aa8b68b066efbe9490d3a2685
Massimiliano Moraca <massimilianomoraca@gmail.com>
2022-09-16 18:41:51 +0200

    Update google_chrome_installation.sh

--- google_chrome_installation.sh
+++ google_chrome_installation.sh
@@ -10,6 +10,6 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key ad
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt update
 sudo apt install google-chrome-stable -y
-google-chrome --version
 
 echo  "--> Google Chrome installation | END"
+google-chrome --version
