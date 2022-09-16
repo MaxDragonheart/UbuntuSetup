cf813b958006c0478b2d3d6b88bd412fdc913936
Massimiliano Moraca <massimilianomoraca@gmail.com>
2022-09-16 15:40:36 +0200

    Create development_setup.sh

---
+++ development_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Install develop setting up | START"
+sudo apt update && sudo apt upgrade -y
+
+echo "--> Install Git"
+sudo apt install -y git
+
+echo "--> Install Python's packages for developing"
+python3 -V
+sudo apt install -y python3-pip build-essential libssl-dev libffi-dev python3-dev
+
+echo "--> Update pip"
+pip3 install --upgrade pip
+export PATH="/home/max/.local/bin:$PATH"
+
+echo "Install develop setting up | END"
