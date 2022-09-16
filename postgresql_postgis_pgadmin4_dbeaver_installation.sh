a88c2c132950c33acb8f471c8bc3c9ff4423d132
Massimiliano Moraca <massimilianomoraca@gmail.com>
2022-09-16 19:15:39 +0200

    Update postgresql_postgis_pgadmin4_dbeaver_installation.sh

--- postgresql_postgis_pgadmin4_dbeaver_installation.sh
+++ postgresql_postgis_pgadmin4_dbeaver_installation.sh
@@ -5,8 +5,8 @@ sudo apt update && sudo apt upgrade -y
 
 echo "--> Install PostreSQL"
 sudo apt install wget ca-certificates
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt>
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -c>
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y postgresql postgresql-contrib
 
@@ -14,8 +14,8 @@ echo "--> Install PostGIS"
 sudo apt install -y postgis
 
 echo "--> Install pgAdmin4"
-sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb>
-sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-ke>
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y pgadmin4
 
