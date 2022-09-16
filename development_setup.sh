a24855c4df9273a4ab0c39d5a35ee51787ec00ed
Massimiliano Moraca <massimilianomoraca@gmail.com>
2022-09-16 22:29:19 +0200

    Update development_setup.sh

--- development_setup.sh
+++ development_setup.sh
@@ -8,7 +8,7 @@ sudo apt install -y git
 
 echo "--> Install Python's packages for developing"
 python3 -V
-sudo apt install -y python3-pip build-essential libssl-dev libffi-dev python3-dev binutils libproj-dev gdal-bin
+sudo apt install -y python3-pip python3-venv build-essential libssl-dev libffi-dev python3-dev binutils libproj-dev gdal-bin
 
 echo "--> Update pip"
 pip3 install --upgrade pip
