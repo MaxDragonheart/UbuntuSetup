5fa3104ede5716828fbd9ba1286a4c87c161298a
MaxDragonheart <massimilianomoraca@gmail.com>
2023-08-04 16:54:49 +0200

    minor fixes

---
+++ video_and_media.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Install video&media | START"
+sudo apt update && sudo apt upgrade -y
+
+echo "--> Install media codecs"
+sudo apt install ubuntu-restricted-extras -y
+
+echo "--> Install VLC"
+sudo add-apt-repository ppa:videolan/stable-daily -y
+sudo apt-get update -y
+sudo apt install vlc -y
+
+echo "--> Install OBS Studio"
+sudo apt install obs-studio -y
+
+echo "--> Install GIMP"
+sudo apt install gimp -y
+
+echo "--> Install Inkscape"
+sudo apt install inkscape -y
+
+echo "Install video&media | END"
