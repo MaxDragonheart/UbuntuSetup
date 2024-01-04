994548cf3b1d1b1f55ad090b09413eb249c4d2dd
Massimiliano Moraca <massimilianomoraca@gmail.com>
2024-01-04 12:22:11 +0100

    Update development_setup.sh

--- development_setup.sh
+++ development_setup.sh
@@ -47,6 +47,6 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Install development settings up | END"
