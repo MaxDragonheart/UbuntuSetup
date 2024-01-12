542bae240c6741129bf537227f5b6543fd15909d
Massimiliano Moraca <massimilianomoraca@gmail.com>
2024-01-12 13:48:57 +0100

    Update and rename dev_software_installation.sh to developer_software_installation.sh
    Added GitHub Desktop

--- developer_software_installation.sh
+++ developer_software_installation.sh
@@ -24,4 +24,10 @@ sudo snap install insomnia
 echo "--> Install FileZilla"
 sudo apt install -y filezilla
 
+echo "--> Install GitHub Desktop"
+wget -qO - https://mirror.mwt.me/shiftkey-desktop/gpgkey | gpg --dearmor | sudo tee /etc/apt/keyrings/mwt-desktop.gpg > /dev/null
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/mwt-desktop.gpg] https://mirror.mwt.me/shiftkey-desktop/deb/ any main" > /etc/apt/sources.list.d/mwt-desktop.list'
+sudo apt update
+sudo apt install github-desktop
+
 echo "Install IDE | END"
